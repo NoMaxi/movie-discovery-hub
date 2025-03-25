822ae3aa79ab1f7904e69f3d65884f1c23023cb5
maksym_novik <maksym_novik@epam.com>
2025-03-25 03:43:39 +0200

    [Module 2] Configure coverage for Jest

--- jest.config.ts
+++ jest.config.ts
@@ -14,6 +14,31 @@ const config: Config = {
     moduleNameMapper: {
         "\\.(css|less|scss|sass)$": "identity-obj-proxy",
     },
+
+    // Coverage
+    collectCoverage: true,
+    coverageDirectory: "coverage",
+    coverageProvider: "v8",
+    coverageReporters: ["text", "lcov", "html"],
+    collectCoverageFrom: [
+        "src/**/*.{ts,tsx}",
+        "!src/**/*.d.ts",
+        "!src/**/*.test.{ts,tsx}",
+        "!src/**/*.spec.{ts,tsx}",
+        "!src/**/*.cy.{ts,tsx}",
+        "!src/**/index.{ts,tsx}",
+        "!src/types/**",
+        "!src/main.tsx",
+        "!src/constants.ts",
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80,
+        },
+    },
 };
 
 export default config;
