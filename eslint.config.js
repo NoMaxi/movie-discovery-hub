3a582641158fffd3f240f9e4b41f9e1e1a99be5f
maksym_novik <maksym_novik@epam.com>
2025-03-25 03:57:13 +0200

    [Module 2] Make ESLint ignore `/coverage` folder

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import testingLibrary from "eslint-plugin-testing-library";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-    { ignores: ["dist"] },
+    { ignores: ["dist", "coverage"] },
     {
         extends: [js.configs.recommended, ...tseslint.configs.recommended],
         files: ["**/*.{ts,tsx}"],
