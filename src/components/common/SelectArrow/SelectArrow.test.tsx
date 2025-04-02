aa20cf507940dc4a4cd71a546f008760a2aa7686
maksym_novik <maksym_novik@epam.com>
2025-04-02 23:47:19 +0300

    [Module 3] Add SelectArrow common component

---
+++ src/components/common/SelectArrow/SelectArrow.test.tsx
@@ -0,0 +1,46 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import SelectArrow from "./SelectArrow";
+
+describe("SelectArrow", () => {
+    test("Should render correctly", () => {
+        const { asFragment } = render(<SelectArrow />);
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    test("Should render an SVG element", () => {
+        render(<SelectArrow />);
+        const svgElement = screen.getByTestId("select-arrow-svg");
+
+        expect(svgElement).toBeInTheDocument();
+        expect(svgElement.tagName.toLowerCase()).toBe("svg");
+    });
+
+    test("Should have correct CSS classes applied to the SVG", () => {
+        render(<SelectArrow />);
+        const svgElement = screen.getByTestId("select-arrow-svg");
+
+        expect(svgElement).toHaveClass("w-3");
+        expect(svgElement).toHaveClass("h-3");
+        expect(svgElement).toHaveClass("fill-[var(--color-primary)]");
+    });
+
+    test("Should have correct SVG attributes", () => {
+        render(<SelectArrow />);
+        const svgElement = screen.getByTestId("select-arrow-svg");
+
+        expect(svgElement).toHaveAttribute("xmlns", "http://www.w3.org/2000/svg");
+        expect(svgElement).toHaveAttribute("viewBox", "0 0 10 10");
+        expect(svgElement).toHaveAttribute("aria-hidden", "true");
+    });
+
+    test("Should contain a path element with correct shape data", () => {
+        render(<SelectArrow />);
+        const pathElement = screen.getByTestId("select-arrow-path");
+
+        expect(pathElement).toBeInTheDocument();
+        expect(pathElement.tagName.toLowerCase()).toBe("path");
+        expect(pathElement).toHaveAttribute("d", "M0 0 L10 0 L5 7 z");
+    });
+});
---
+++ src/components/common/SelectArrow/SelectArrow.tsx
@@ -0,0 +1,13 @@
+const SelectArrow = () => (
+    <svg
+        data-testid="select-arrow-svg"
+        className="w-3 h-3 fill-[var(--color-primary)]"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 10 10"
+        aria-hidden="true"
+    >
+        <path data-testid="select-arrow-path" d="M0 0 L10 0 L5 7 z" />
+    </svg>
+);
+
+export default SelectArrow;
