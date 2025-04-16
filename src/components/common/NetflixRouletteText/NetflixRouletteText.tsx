f5b69534d1cc0663757b69d0c3ce1defbbbf12f7
maksym_novik <maksym_novik@epam.com>
2025-04-16 22:41:21 +0300

    [Module 5] Create NetflixRouletteText component

---
+++ src/components/common/NetflixRouletteText/NetflixRouletteText.tsx
@@ -0,0 +1,6 @@
+export const NetflixRouletteText = () => (
+    <span className="text-[var(--color-primary)] group">
+        <span className="font-black group-hover:text-blue-500 transition-colors duration-300">netflix</span>
+        <span className="font-medium group-hover:text-red-500 transition-colors duration-300">roulette</span>
+    </span>
+);
