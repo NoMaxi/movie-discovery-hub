8cd8b101c13c94e21d5bd7906da13a833af39e2f
maksym_novik <maksym_novik@epam.com>
2025-04-17 04:00:07 +0300

    [Module 5] Update ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -8,13 +8,11 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
     { ignores: ["dist", "coverage"] },
+
+    // Base JS/TS/React/Query config
     {
-        extends: [
-            js.configs.recommended,
-            ...tseslint.configs.recommended,
-            "plugin:@tanstack/eslint-plugin-query/recommended",
-        ],
         files: ["**/*.{ts,tsx}"],
+        extends: [js.configs.recommended, ...tseslint.configs.recommended, pluginQuery.configs["flat/recommended"]],
         languageOptions: {
             ecmaVersion: 2020,
             globals: globals.browser,
@@ -22,34 +20,41 @@ export default tseslint.config(
         plugins: {
             "react-hooks": reactHooks,
             "react-refresh": reactRefresh,
-            "@tanstack/query": pluginQuery,
         },
         rules: {
-            ...reactHooks.configs["recommended-latest"].rules,
-            ...pluginQuery.configs["flat/recommended"],
+            ...reactHooks.configs.recommended.rules,
             "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
         },
     },
-    // Jest
+
+    // Testing Library React config
+    {
+        ...testingLibrary.configs["flat/react"],
+        ignores: ["**/*.cy.{ts,tsx}"],
+    },
+
+    // Testing Library DOM config
+    {
+        ...testingLibrary.configs["flat/dom"],
+        ignores: ["**/*.cy.{ts,tsx}"],
+    },
+
+    // Custom Jest/Testing Library overrides
     {
         files: ["**/*.test.{ts,tsx}", "**/*.spec.{ts,tsx}"],
-        plugins: {
-            "testing-library": testingLibrary,
-        },
         languageOptions: {
             globals: globals.jest,
         },
         rules: {
-            ...testingLibrary.configs.react.rules,
             "testing-library/await-async-queries": "error",
             "testing-library/no-await-sync-queries": "error",
             "testing-library/no-debugging-utils": "warn",
             "testing-library/no-dom-import": "off",
+            "testing-library/prefer-screen-queries": "error",
         },
-        ...testingLibrary.configs["flat/dom"],
-        ...testingLibrary.configs["flat/react"],
     },
-    // Cypress
+
+    // Cypress config
     {
         files: ["cypress/**/*.ts", "cypress/**/*.tsx"],
         extends: [...tseslint.configs.recommended],
@@ -68,6 +73,9 @@ export default tseslint.config(
             "@typescript-eslint/no-namespace": "off",
             "no-unused-vars": "off",
             "@typescript-eslint/no-unused-vars": "off",
+            "testing-library/await-async-queries": "off",
+            "testing-library/prefer-screen-queries": "off",
+            "testing-library/no-await-sync-queries": "off",
         },
     },
 );
