6a54d918f163ee06825f3b1140f4f83731c0f4f5
maksym_novik <maksym_novik@epam.com>
2025-04-23 02:58:53 +0300

    [Module 6] Update Cypress package and configuration

---
+++ cypress/support/e2e.ts
@@ -0,0 +1,9 @@
+// This is the E2E support file.
+// It's processed and loaded automatically before your test files.
+// You can put global configuration and behavior that modifies Cypress here.
+
+// Import commands.ts using ES2015 syntax:
+import './commands';
+
+// Alternatively, you can use CommonJS syntax:
+// require('./commands')
