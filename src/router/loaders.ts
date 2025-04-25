befd165f820bff9a838ede348f1a42cbc131a83c
maksym_novik <maksym_novik@epam.com>
2025-04-25 04:04:38 +0300

    [Module 6] Add non-number movie id handling during movie details loading

--- src/router/loaders.ts
+++ src/router/loaders.ts
@@ -3,5 +3,10 @@ import { movieService } from "@/services/movieService";
 
 export const movieDetailsLoader: LoaderFunction = async ({ params }) => {
     const movieId = Number(params.movieId);
+
+    if (!movieId || Number.isNaN(movieId)) {
+        throw new Error("Invalid movie id");
+    }
+
     return { movieDetails: await movieService.getMovieById(movieId) };
 };
