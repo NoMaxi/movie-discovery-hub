c11825ac72cc8d267671364b0c59504c2db617c4
maksym_novik <maksym_novik@epam.com>
2025-04-30 22:31:47 +0300

    [Module 7] Add AddMovieFormPage component to routes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,13 +4,23 @@ import { MovieListPage } from "@/pages/MovieListPage/MovieListPage";
 import { SearchFormPage } from "@/pages/SearchFormPage/SearchFormPage";
 import { MovieDetailsPage } from "@/pages/MovieDetailsPage/MovieDetailsPage";
 import { MovieDetailsError } from "@/components/MovieDetailsError/MovieDetailsError";
+import { AddMovieFormPage } from "@/pages/AddMovieFormPage/AddMovieFormPage";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         Component: MovieListPage,
         children: [
-            { index: true, Component: SearchFormPage },
+            {
+                path: "/",
+                Component: SearchFormPage,
+                children: [
+                    {
+                        path: "/new",
+                        Component: AddMovieFormPage,
+                    },
+                ],
+            },
             {
                 path: "/:movieId",
                 Component: MovieDetailsPage,
