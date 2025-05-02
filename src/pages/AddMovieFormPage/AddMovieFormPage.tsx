42725484e831cb66a733bdc4e2493183ac5a192b
maksym_novik <maksym_novik@epam.com>
2025-05-02 04:16:04 +0300

    [Module 7] Update AddMovieFormPage component

--- src/pages/AddMovieFormPage/AddMovieFormPage.tsx
+++ src/pages/AddMovieFormPage/AddMovieFormPage.tsx
@@ -1,24 +1,29 @@
-import { Dialog } from "@/components/common/Dialog/Dialog";
-import { useNavigate, useSearchParams } from "react-router-dom";
-import { Movie, MovieFormData } from "@/types/common";
+import { useState } from "react";
 import { movieService } from "@/services/movieService";
+import { MovieFormData } from "@/types/common";
+import { useNavigateWithSearchParams } from "@/hooks/useNavigateWithSearchParams/useNavigateWithSearchParams";
+import { ErrorMessage } from "@/components/common/ErrorMessage/ErrorMessage";
+import { Dialog } from "@/components/common/Dialog/Dialog";
 import { MovieForm } from "@/components/MovieForm/MovieForm";
 
 export const AddMovieFormPage = () => {
-    const [searchParams] = useSearchParams();
-    const navigate = useNavigate();
-
-    const navigateWithSearchParams = (path: string) => {
-        const searchParamsString = searchParams.toString();
-        navigate(`${path}${searchParamsString ? `?${searchParamsString}` : ""}`);
-    };
+    const navigateWithSearchParams = useNavigateWithSearchParams();
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
     const handleSubmit = async (formData: MovieFormData) => {
+        setErrorMessage(null);
+        setIsSubmitting(true);
+
         try {
-            const movie: Movie = await movieService.createMovie(formData);
+            const movie = await movieService.createMovie(formData);
+            window.scrollTo({ top: 0, behavior: "smooth" });
             navigateWithSearchParams(`/${movie.id}`);
         } catch (error) {
-            console.error("Error creating movie:", error);
+            console.error("Error creating a movie:", error);
+            setErrorMessage("Failed to create a movie. Please try again.");
+        } finally {
+            setIsSubmitting(false);
         }
     };
 
@@ -26,12 +31,15 @@ export const AddMovieFormPage = () => {
         navigateWithSearchParams("/");
     };
 
+    const showErrorMessage = !isSubmitting && !!errorMessage;
+
     return (
-        <Dialog
-            title="Add Movie"
-            children={<MovieForm onSubmit={handleSubmit} />}
-            onClose={handleClose}
-            className="max-w-[976px]"
-        />
+        <Dialog title="Add Movie" onClose={handleClose} className="max-w-[976px]">
+            {showErrorMessage && (
+                <ErrorMessage message={errorMessage} onDismiss={() => setErrorMessage(null)} className="pb-6" />
+            )}
+
+            <MovieForm onSubmit={handleSubmit} isLoading={isSubmitting} resetMode="clear" />
+        </Dialog>
     );
 };
